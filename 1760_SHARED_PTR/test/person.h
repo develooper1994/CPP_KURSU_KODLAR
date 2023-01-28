b05ef85608796fec8b17252bcf41515c882c2f29
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-01-28 08:35:14 +0300

    Update person.h

--- 1760_SHARED_PTR/test/person.h
+++ 1760_SHARED_PTR/test/person.h
@@ -8,14 +8,16 @@ class Person {
 public:
 	Person(std::string name, std::string surname) :
 		m_name{ std::move(name) }, m_surname{ std::move(surname) } {}
-	friend std::ostream& operator<<(std::ostream&os, const Person&p)
+	friend std::ostream& operator<<(std::ostream& os, const Person& p)
 	{
 		return os << p.m_name << ' ' << p.m_surname;
 	}
 
+	auto operator<=>(const Person& p)const = default;
 	Person& set_name(const std::string& name)
 	{
 		m_name = name;
+		return *this;
 	}
 
 	std::string get_name()const { return m_name; }
