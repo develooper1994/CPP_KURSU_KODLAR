c845229785b30c04ee5740eefc7e1ecaba58a5d2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-02-14 17:45:23 +0300

    Update smf_06.cpp

--- 0170_CLASSES/special_member_functions/smf_06.cpp
+++ 0170_CLASSES/special_member_functions/smf_06.cpp
@@ -7,10 +7,13 @@ public:
 
 class Nec_ {
 public:
+	//default ctor. is not declared
 	~Nec_() = default;
+
+	//copy members are implicitly deleted
 	Nec_(const Nec_&) = delete; 
 	Nec_& operator=(const Nec_&) = delete; 
+	
 	Nec_(Nec_&&); //user declared
-
 	//move assignment is not declared
 };
