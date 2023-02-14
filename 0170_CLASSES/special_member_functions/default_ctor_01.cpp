a4cbdbb23a387385f5241b943c9e015b633c8208
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-02-14 18:54:07 +0300

    Create default_ctor_01.cpp

---
+++ 0170_CLASSES/special_member_functions/default_ctor_01.cpp
@@ -0,0 +1,14 @@
+// A, B and C are types
+
+class Myclass {
+public:
+	//default constructor defaulted by the compiler
+	Myclass() : m_a(), m_b(), m_c()
+	{
+	}
+		
+private:
+	A m_a;
+	B m_b;
+	C m_c;
+};
