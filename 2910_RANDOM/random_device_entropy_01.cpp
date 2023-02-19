80c8da58d6ea7ebd7836bce99160c13a78f67c26
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-02-19 08:50:00 +0300

    Update random_device_entropy_01.cpp

--- 2910_RANDOM/random_device_entropy_01.cpp
+++ 2910_RANDOM/random_device_entropy_01.cpp
@@ -4,5 +4,7 @@
 int main()
 {
 	std::random_device device;
-	std::cout << "entropy : " << device.entropy() << "\n";
+	std::cout << "entropy : " << device.entropy() << '\n';
+
+	//A deterministic random number generator (e.g. a pseudo-random engine) has entropy zero.
 }
