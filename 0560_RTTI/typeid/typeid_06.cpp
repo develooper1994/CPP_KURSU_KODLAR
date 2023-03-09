c2dd946ebe39d2551ddfc14c58804e28dca67a35
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-03-09 18:52:33 +0300

    Update typeid_06.cpp

--- 0560_RTTI/typeid/typeid_06.cpp
+++ 0560_RTTI/typeid/typeid_06.cpp
@@ -4,8 +4,11 @@
 int main()
 {
 	//type_info sınıfının varsayılan kurucu islevi yok
-	//type_info x; //gecersiz 
+	//std::type_info x; //gecersiz 
 
 	//type_info sınıfının kopyalayan kurucuy işlevi delete edilmiş.
 	//auto x = typeid(20); //gecersiz
+	//std::type_info& tr = typeid(int); //gecersiz, const & olmalı
+	const std::type_info& tr1 = typeid(int); //gecerli
+	auto& tr2 = typeid(int); //gecerli
 }
