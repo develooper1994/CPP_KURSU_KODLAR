7b2456b84437eb21fc455bd86908262b6b6aef93
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-04-19 10:39:26 +0300

    Update rc_01.cpp

--- 3370_CONCEPTS/requires_clause/rc_01.cpp
+++ 3370_CONCEPTS/requires_clause/rc_01.cpp
@@ -9,15 +9,21 @@ struct A
     using value_type = int;
 };
 
+struct B{};
+
 template <typename T>
-    requires nec<T>
+requires nec<T>
 class Myclass {
 
 };
+
+void func(nec auto);
 
 int main()
 {
     //Myclass<int> x; // Syntax Error
 
-    Myclass<A> x; // Valid
+    Myclass<A> x; // ok
+    func(A{}); //ok
+    //func(B{}); // error
 }
