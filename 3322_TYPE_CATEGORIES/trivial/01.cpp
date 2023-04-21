b86da0688a4a1802a3b3be4cbf3645b06a895e2c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-04-21 16:10:19 +0300

    Update 01.cpp

--- 3322_TYPE_CATEGORIES/trivial/01.cpp
+++ 3322_TYPE_CATEGORIES/trivial/01.cpp
@@ -19,3 +19,27 @@ struct T3 : T2 {
 };
 //trivial sınıfların taban sınıfları olabilir özel üye fonksiyonları default edilebilir.
 static_assert(std::is_trivial_v<T3>); //Ok
+
+
+struct T4 {
+public:
+    int a;
+protected: 
+    int b;
+private:
+    int c;
+};
+
+//trivial sınıfların non-static veri elemanları private ya da protected olabilir.
+static_assert(std::is_trivial_v<T4>); //Ok
+
+
+struct T5 {
+    T1 t1;
+    T2 t2;
+    T3 t3;
+    T4 t4;
+};
+
+//trivial sınıfların non-static veri elemanları trivial türlerden olabilir
+static_assert(std::is_trivial_v<T5>); //Ok
