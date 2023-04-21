4a92e911a78bf9ce8c4ff530d59b88eae1da995a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-04-21 16:53:34 +0300

    Create 02.cpp

---
+++ 3322_TYPE_CATEGORIES/trivial/02.cpp
@@ -0,0 +1,40 @@
+#include <type_traits>
+
+
+
+struct Nt1 {
+	Nt1();
+};
+
+// default ctor. user provided Nt1 trivial değil
+//static_assert(std::is_trivial_v<Nt1>);
+
+struct Nt2 {
+	Nt1 nt1;
+};
+
+// Non trivial elemanı var. trivial değil
+//static_assert(std::is_trivial_v<Nt2>);
+
+struct Nt3 : Nt1{
+	
+};
+
+//static_assert(std::is_trivial_v<Nt3>);
+
+struct Nt4 {
+	Nt4();
+};
+
+Nt4::Nt4() = default;
+
+//static_assert(std::is_trivial_v<Nt4>);
+
+struct Nt4 {
+public:
+	Nt4();
+};
+
+Nt4::Nt4() = default;
+// default ctor daha sonra default edilirse trivial sınıf olmuyor
+//static_assert(std::is_trivial_v<Nt4>);
