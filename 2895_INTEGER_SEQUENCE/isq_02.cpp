73fb85322d2f9321074733cfd5176b6c888fb954
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-04-26 12:47:07 +0300

    Update isq_02.cpp

--- 2895_INTEGER_SEQUENCE/isq_02.cpp
+++ 2895_INTEGER_SEQUENCE/isq_02.cpp
@@ -15,4 +15,5 @@ int main()
     print_seq(std::index_sequence<2u, 3u, 5u, 7u, 11u>{});
     print_seq(std::make_integer_sequence<int, 20>{});
     print_seq(std::make_index_sequence<10u>{});
+    print_seq(std::index_sequence_for<char, short, int, long>{});
 }
