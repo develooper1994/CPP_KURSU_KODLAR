5b0898379ecc73ab8086fad4897bb9c0e72765bd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-05-19 16:23:12 +0300

    Update mandatory_02.cpp

--- 0217_COPY_ELISION/mandatory_02.cpp
+++ 0217_COPY_ELISION/mandatory_02.cpp
@@ -7,19 +7,19 @@ struct A {
 	}
 
 	A(const A&) = delete;
-	A(A &&) = delete;
+	A(A&&) = delete;
 };
 
 void func(A) { }
 
 A foo()
 {
-	return A{3};
+	return A{ 3 };
 }
 
 int main()
 {
-	A ax = A{1};
-	func(A{2});
-	A ay = foo();
+	A ax = A{ 1 }; //mandatory copy elision
+	func(A{ 2 }); //mandatory copy elision
+	A ay = foo(); //mandatory copy elision
 }
