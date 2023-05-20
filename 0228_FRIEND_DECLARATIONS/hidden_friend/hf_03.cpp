acd079a594dced73e9df3ce64d61c17b93954a4f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-05-21 00:56:48 +0300

    Update hf_03.cpp

--- 0228_FRIEND_DECLARATIONS/hidden_friend/hf_03.cpp
+++ 0228_FRIEND_DECLARATIONS/hidden_friend/hf_03.cpp
@@ -1,1 +1,12 @@
-//...
+struct Nec {
+    Nec(int) {}
+    friend void func(Nec) {};
+};
+
+int main()
+{
+    Nec x{5};
+    func(x);    // gecerli (ADL)
+    func(7);    // geçersiz: func ismi bulunamadi
+    ::func(x);  // geçersiz 
+}
