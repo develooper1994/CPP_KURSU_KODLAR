dee5d6336a8487034ab9f1bbc41e8423055a3221
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-05-21 14:07:45 +0300

    Update adl_02.cpp

--- 0340_NAMESPACE/adl/adl_02.cpp
+++ 0340_NAMESPACE/adl/adl_02.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	std::cout << "Necati Ergin\n"; 
-	operator<<(std::cout, "Kaan Aslan\n"); 
+	operator<<(std::cout, "Tekin Aslan\n"); 
 	//std::cout << endl; 
 	endl(std::cout); 
 	//(endl)(std::cout); 
@@ -14,7 +14,7 @@ int main()
 std::cout << "Necati Ergin\n"; 
 Burada çağrılan operator<< işlevi std isim alanı içinde bildirilmesine karşın isim arama ile bulunuyor.
 Fonksiyona gönderilen argüman olan std::cout std isim alanı içinde olduğundan operator<< işlev ismi std isim alanında da aranıyor.
-operator<<(std::cout, "Kaan Aslan\n");   
+operator<<(std::cout, "Tekin Aslan\n");   
 Aynı durum burada da geçerli. operator<< ADL nedeniyle std isim alanında aranıyor.
 std::cout << endl; 
 Burada ADL devreye girmiyor. Çünkü çağrılan işlev endl işlevi değil. endl operator<< işlevine argüman olarak gönderiliyor.
