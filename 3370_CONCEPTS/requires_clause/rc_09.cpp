2d8923550c03fc4a5565886d653c8dc16c1d26ec
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-05-29 09:20:42 +0300

    Update rc_09.cpp

--- 3370_CONCEPTS/requires_clause/rc_09.cpp
+++ 3370_CONCEPTS/requires_clause/rc_09.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <string>
 
 template <typename T>
 concept nec = requires(T x)
 {
     std::cout << x;
-    std::cout >> x;
+    std::cin >> x;
 };
 
 struct A {};
 
 int main()
 {
-    static_assert(nec<A>); //invalid
+    static_assert(nec<std::string>); //valid
+    static_assert(nec<A>); //error
 }
