f87f042ea50cce152aea06b1767ea16ece512cba
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-05-29 09:17:56 +0300

    Update rc_06.cpp

--- 3370_CONCEPTS/requires_clause/rc_06.cpp
+++ 3370_CONCEPTS/requires_clause/rc_06.cpp
@@ -1,11 +1,12 @@
 #include <memory>
 #include <optional>
 
-
 template <typename T>
 concept nec = requires(T p)
 {
-    p == nullptr; // unevaluated expression.ifadenın kendisi geçerli olmalı.true false olması önemsiz
+    p == nullptr; //unevaluated expression.
+    //ifadenın kendisi geçerli olmalı.
+    //ifadenin değerinin true ya da false olması önemsiz
 };
 
 int main() {
@@ -13,6 +14,7 @@ int main() {
     static_assert(nec<int*>); // valid
     static_assert(nec<std::unique_ptr<int>>);  //valid
 
-    static_assert(nec<std::optional<int>>);  //invalid
+    static_assert(nec<std::nullptr_t>);  // valid
     static_assert(nec<std::optional<int*>>);  // valid
+    static_assert(nec<std::optional<int>>);  //invalid
 }
