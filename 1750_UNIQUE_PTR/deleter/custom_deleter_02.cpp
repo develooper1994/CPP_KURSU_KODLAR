b85d70c18118787276058f6ef933bc66ccfb0569
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-06-09 18:35:25 +0300

    Update custom_deleter_02.cpp

--- 1750_UNIQUE_PTR/deleter/custom_deleter_02.cpp
+++ 1750_UNIQUE_PTR/deleter/custom_deleter_02.cpp
@@ -11,6 +11,10 @@ int main()
 		delete p;
 	};
 
-	//unique_ptr<string, decltype(f)> uptr{ new string{"Necati"}, f };
-	unique_ptr<string, decltype(f)> uptr{ new string{"Necati"} }; //C++20
+	unique_ptr<string, decltype(f)> uptr1{ new string{"Necati"}, f };
+	unique_ptr<string, decltype(f)> uptr2{ new string{"Necati"} }; //C++20
+	unique_ptr<string, decltype([](string* p) {
+		cout << "lambda deleter deletes the string at the address " << p << '\n';
+		delete p;
+		})> uptr3{ new string{"Necati"} }; //C++20
 }
