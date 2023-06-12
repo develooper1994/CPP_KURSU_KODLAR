01927205074c2e45b557aa0d876f8ea3e1f50a01
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-06-12 17:01:27 +0300

    Update to_array_01.cpp

--- 0930_ARRAY/to_array_01.cpp
+++ 0930_ARRAY/to_array_01.cpp
@@ -10,6 +10,6 @@ int main()
     auto ar = std::to_array("necati");
     static_assert(ar.size() == 7);
     std::cout << typeid(ar).name() << "\n";
-    constexpr auto b = std::is_same_v<std::array<char, 7>, decltype(ar)>;
+    constexpr auto b = std::is_same_v<std::array<char, 7u>, decltype(ar)>;
     std::cout << std::boolalpha << "b = " << b << "\n";
 }
