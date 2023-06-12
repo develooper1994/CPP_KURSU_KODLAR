f7f33fa569b6d0d2de8aec1f945717aef88f5ec1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-06-12 17:04:46 +0300

    Update and rename 0930_ARRAY/to_array_04.cpp to 0930_ARRAY/to_array/to_array_04.cpp

--- 0930_ARRAY/to_array/to_array_04.cpp
+++ 0930_ARRAY/to_array/to_array_04.cpp
@@ -5,5 +5,5 @@ int main()
     auto ar = std::to_array<std::pair<char, double>>(
         { { 'A', 1.1}, { 'B', 2.2 }, { 'C', 3.3} });
     
-    static_assert(ar.size() == 3);
+    static_assert(ar.size() == 3u);
 }
