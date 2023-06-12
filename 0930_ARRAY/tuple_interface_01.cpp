2b7d7ede239352588c5600039916a01743e7de18
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-06-12 17:06:15 +0300

    Update tuple_interface_01.cpp

--- 0930_ARRAY/tuple_interface_01.cpp
+++ 0930_ARRAY/tuple_interface_01.cpp
@@ -7,14 +7,14 @@ int main()
 
 	array<int, 5> ax{ 1, 4, 5, 6, 7 };
 	using atype = decltype(ax);
-	int idx = 3;
-	constexpr int index = 3;
+	int idx{3};
+	constexpr int index{3};
 
-	cout << get<0>(ax) << "\n";
-	//cout << get<idx>(ax) << "\n"; //gecersiz
-	cout << get<index>(ax) << "\n";
-	cout << tuple_size<atype>::value<< "\n";
-	cout << tuple_size_v<atype><< "\n";
+	cout << get<0>(ax) << '\n';
+	//cout << get<idx>(ax) << '\n'; //invalid
+	cout << get<index>(ax) << '\n';
+	cout << tuple_size<atype>::value<< '\n';
+	cout << tuple_size_v<atype><< '\n';
 	tuple_element<1, atype>::type ival = 34;
 	// tuple_element_t<9, atype> ival = 34;
 }
