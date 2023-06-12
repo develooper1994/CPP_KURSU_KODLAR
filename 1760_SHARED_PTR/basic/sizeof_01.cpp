db299d975370d7105510517d6a8fd046ea6f7d52
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-06-12 18:44:07 +0300

    Update sizeof_01.cpp

--- 1760_SHARED_PTR/basic/sizeof_01.cpp
+++ 1760_SHARED_PTR/basic/sizeof_01.cpp
@@ -5,8 +5,8 @@
 int main()
 {
 	using namespace std;
-	shared_ptr<string> sptr{ new string{"necati"}};
+	shared_ptr<string> sptr{ new string{"necati"} };
 
-	cout << "sizeof(string *) = " << sizeof(string *) << "\n";
-	cout << "sizeof(sptr) = " << sizeof(sptr) << "\n";
+	cout << "sizeof(string *) = " << sizeof(string*) << '\n';
+	cout << "sizeof(sptr) = " << sizeof(sptr) << '\n';
 }
