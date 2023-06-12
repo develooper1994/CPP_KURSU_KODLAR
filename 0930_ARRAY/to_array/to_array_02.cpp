229c0cf6d1f06edf975f2c23868b40460f5c726a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-06-12 17:03:18 +0300

    Update and rename 0930_ARRAY/to_array_02.cpp to 0930_ARRAY/to_array/to_array_02.cpp

--- 0930_ARRAY/to_array/to_array_02.cpp
+++ 0930_ARRAY/to_array/to_array_02.cpp
@@ -4,5 +4,5 @@
 int main()
 {
     auto ar = std::to_array({10, 20, 30, 40, 50 });
-    static_assert(std::is_same_v<decltype(ar), std::array<int, 5>>);
+    static_assert(std::is_same_v<decltype(ar), std::array<int, 5u>>);
 }
