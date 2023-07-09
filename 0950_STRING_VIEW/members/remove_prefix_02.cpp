128bf761e7fa6c86a7ea55ae5fef6824610085dd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-07-09 07:48:28 +0300

    Update and rename remove_prefix2.cpp to remove_prefix_02.cpp

--- 0950_STRING_VIEW/members/remove_prefix_02.cpp
+++ 0950_STRING_VIEW/members/remove_prefix_02.cpp
@@ -9,8 +9,8 @@ int main()
 	str.remove_prefix(3);
 	str.remove_suffix(4);
 
-	std::cout << "the length of " << str << " is " << std::strlen(str.data()) << "\n";
-	std::cout << "the length of " << str << " is " << str.length() << "\n";
+	std::cout << "the length of " << str << " is " << std::strlen(str.data()) << '\n';
+	std::cout << "the length of " << str << " is " << str.length() << '\n';
 	std::cout << "str.data() is " << str.data() << '\n';
 	std::cout << "str is [" << str << "]\n";
 }
