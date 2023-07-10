7de629f42a339f2ba1a1e84cc2004ff0d008dcae
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-07-10 17:38:39 +0300

    Update and rename 2920_OPTIONAL/operator_bool_01.cpp to 2920_OPTIONAL/basic/operator_bool_01.cpp

--- 2920_OPTIONAL/basic/operator_bool_01.cpp
+++ 2920_OPTIONAL/basic/operator_bool_01.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 
-std::optional<std::string> getMiddleName(const std::string& rname)
+std::optional<std::string> get_middle_name(const std::string& rname)
 {
 	//return {};
 	return "alican";
@@ -11,10 +11,12 @@ std::optional<std::string> getMiddleName(const std::string& rname)
 
 int main()
 {
+	using namespace std;
+
 	std::string str;
 	///code
 
-	if (auto ostr = getMiddleName(str); ostr)
+	if (auto ostr = get_middle_name(str); ostr)
 		std::cout << *ostr << '\n';
 	else
 		std::cout << "ostr bos durumda\n";
