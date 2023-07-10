9b196411f65d8f15f9d6e98cd77e8070660c3969
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-07-10 18:02:07 +0300

    Update and rename 2920_OPTIONAL/move_02.cpp to 2920_OPTIONAL/basic/move_02.cpp

--- 2920_OPTIONAL/basic/move_02.cpp
+++ 2920_OPTIONAL/basic/move_02.cpp
@@ -11,10 +11,12 @@ struct A {
 
 int main()
 {
-	std::optional<A> op1;
+	using namespace std;
+
+	optional<A> op1;
 	op1.emplace(); //default ctor
-	std::optional<A> op2{ std::move(op1) }; //A's move ctor.
-	std::cout << std::boolalpha;
-	std::cout << op1.has_value() << "\n";
-	std::cout << op2.has_value() << "\n";
+	optional<A> op2{ std::move(op1) }; //A's move ctor.
+	cout << boolalpha;
+	cout << op1.has_value() << '\n';
+	cout << op2.has_value() << '\n';
 }
