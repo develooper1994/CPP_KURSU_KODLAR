cd9be89a747d90e7ec0bc1f5003e1e8b39dc21dd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-07-10 17:42:32 +0300

    Update and rename 2920_OPTIONAL/value_or_01.cpp to 2920_OPTIONAL/basic/value_or_01.cpp

--- 2920_OPTIONAL/basic/value_or_01.cpp
+++ 2920_OPTIONAL/basic/value_or_01.cpp
@@ -1,12 +1,12 @@
 #include <optional>
 #include <string>
 #include <iostream>
- 
-void display_e_mail(const std::optional<std::string> &op)
+
+void display_e_mail(const std::optional<std::string>& op)
 {
-	std::cout << "e posta adresi : " << op.value_or("belirtilmemis") << "\n";
+	std::cout << "e posta adresi : " << op.value_or("belirtilmemis") << '\n';
 }
- 
+
 int main()
 {
 	std::optional<std::string> e_mail_address{ "necati@gmail.com" };
