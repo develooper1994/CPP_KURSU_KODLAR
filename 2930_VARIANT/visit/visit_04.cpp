c2a4fbe8db9f0a2061337eb19c98f56f2521249f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-07-11 15:38:05 +0300

    Update visit_04.cpp

--- 2930_VARIANT/visit/visit_04.cpp
+++ 2930_VARIANT/visit/visit_04.cpp
@@ -8,7 +8,7 @@ int main()
 
 	variant<int, string, double> vx{ "mustafa" };
 
-	cout << get<1>(vx) << "\n";
+	cout << get<1>(vx) << '\n';
 	auto f = [](auto& val) {
 		val = std::remove_reference_t<decltype(val)>{};
 	};
@@ -16,11 +16,11 @@ int main()
 	visit(f, vx);
 	cout << get<1>(vx).size() << '\n';
 	vx = 999;
-	cout << get<0>(vx)<< '\n';
+	cout << get<0>(vx) << '\n';
 	visit(f, vx);
-	cout << get<0>(vx)<< '\n';
+	cout << get<0>(vx) << '\n';
 	vx = 56.76;
-	cout << get<2>(vx)<< '\n';
+	cout << get<2>(vx) << '\n';
 	visit(f, vx);
-	cout << get<2>(vx)<< '\n';
+	cout << get<2>(vx) << '\n';
 }
