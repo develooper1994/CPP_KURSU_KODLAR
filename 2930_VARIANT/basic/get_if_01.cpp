c8a4a51695e089423e0d2b2bec2fc155d33bf6d5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-07-11 15:18:40 +0300

    Update get_if_01.cpp

--- 2930_VARIANT/basic/get_if_01.cpp
+++ 2930_VARIANT/basic/get_if_01.cpp
@@ -18,36 +18,36 @@ int main()
 
 		if (auto pAge = std::get_if<age_t>(&person))
 		{
-			std::cout << "Age is " << *pAge << "\n";
+			std::cout << "Age is " << *pAge << '\n';
 		}
 		else if (auto pGender = std::get_if<gender_t>(&person))
 		{
-			std::cout << "Gender is " << *pGender << "\n";
+			std::cout << "Gender is " << *pGender << '\n';
 		}
 		else if (auto pName = std::get_if<name_t>(&person))
 		{
-			std::cout << "Name is " << *pName << "\n";
+			std::cout << "Name is " << *pName << '\n';
 		}
 
-		std::cout << "\n";
+		std::cout << '\n';
 
 		person = "Kelami Yilmaz";
 
 		if (auto pAge = std::get_if<idx_age>(&person))
 		{
-			std::cout << "My age is " << *pAge << "\n";
+			std::cout << "My age is " << *pAge << '\n';
 		}
 		else if (auto pGender = std::get_if<idx_gender>(&person))
 		{
-			std::cout << "My gender is " << *pGender << "\n";
+			std::cout << "My gender is " << *pGender << '\n';
 		}
 		else if (auto pName = std::get_if<idx_name>(&person))
 		{
-			std::cout << "Benim adim " << *pName << "\n";
+			std::cout << "Benim adim " << *pName << '\n';
 		}
 	}
 	catch (std::bad_variant_access& e)
 	{
-		std::cout << e.what() << "\n";
+		std::cout << e.what() << '\n';
 	}
 }
