cc1861c2efbf55073caaf713e97e841f19f1bb55
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-07-21 19:20:50 +0300

    Update sb_05.cpp

--- 1820_STRUCTURED_BINDING/sb_05.cpp
+++ 1820_STRUCTURED_BINDING/sb_05.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
+#include <string>
 
 struct Nec {
 
 	Nec() = default;
-	Nec(const Nec& other) : mx{other.mx}, my{other.my}, mz{other.mz}
+	Nec(const Nec& other) : ms1{ other.ms1 }, ms2{ other.ms2 }, ms3{ other.ms3 }
 	{
 		std::cout << "copy ctor\n";
 	}
 
-	Nec(Nec&& other) : mx{ other.mx }, my{ other.my }, mz{ other.mz }
+	Nec(Nec&& other) : ms1{ std::move(other.ms1) }, ms2{ std::move(other.ms2)}, ms3{ std::move(other.ms3 )}
 	{
 		std::cout << "move ctor\n";
 	}
-	int mx{}, my{}, mz{};
+	std::string ms1{ "ahmet" }, ms2{ "mehmet" }, ms3{ "huseyin" };
 };
 
 int main()
 {
-	Nec mynec;
+	Nec nec;
 
-	auto [x1, y1, z1] = mynec; 
-	auto [x2, y2, z2] = std::move(mynec); 
+	auto [x2, y2, z2] = std::move(nec);
+
+	std::cout << "nec.ms1.length() = " << nec.ms1.length() << '\n';
 }
