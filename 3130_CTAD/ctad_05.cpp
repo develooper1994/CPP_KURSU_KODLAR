e3eda02d9e7633d389545ff056a8c239697bcf5a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-07-28 14:45:04 +0300

    Update ctad_05.cpp

--- 3130_CTAD/ctad_05.cpp
+++ 3130_CTAD/ctad_05.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-	std::array x{ 1, 3, 5, 7, 9 };
-	//std::array y{ 2, 3.5, 7.8, 9.1};  //gecersiz
-	//std::array<double> z{ 3.5, 7.8, 9.1};  //gecersiz
+	std::array a1{ 1, 3, 5, 7, 9 };
+	//std::array a2{ 2, 3.5, 7.8, 9.1};  //invalid
+	//std::array<double> a3{ 3.5, 7.8, 9.1};  //invalid
+	//std::array<> a4{ 10, 20, 30, 40 }; //invalid
 }
