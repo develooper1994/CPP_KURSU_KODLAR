aff598a30dfba79a48fc2b3f2b02ed8ab5b36885
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-04 18:50:00 +0300

    Update question_06.cpp

--- 1214_LAMBDA_EXPRESSIONS/questions/question_06.cpp
+++ 1214_LAMBDA_EXPRESSIONS/questions/question_06.cpp
@@ -1,9 +1,13 @@
+// sentaks hatası olan yerleri işaretleyin.
+
 int main()
 {
 	int a[10]{};
 
-	auto f = [a]()mutable {a = nullptr; };
-	auto g = [a = a]()mutable {a = nullptr; };
-	auto h = [a]() {++* a; };
-	auto j = [a = a]() {++* a; };
+	auto f1 = [a]()mutable {a = nullptr; };
+	auto f2 = [a = a](){a = nullptr; };
+	auto f3 = [a = a]()mutable{a = nullptr; };
+	auto f4 = [a]() {++* a; };
+	auto f5 = [a]()mutable {++* a; };
+	auto f6 = [a = a]() {++* a; };
 }
