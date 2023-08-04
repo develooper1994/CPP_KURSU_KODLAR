55c729c0c2ccca7d2565a3b12dcf0c4aec8ee6c5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-04 19:10:24 +0300

    Update init_capture_04.cpp

--- 1214_LAMBDA_EXPRESSIONS/init_capture/init_capture_04.cpp
+++ 1214_LAMBDA_EXPRESSIONS/init_capture/init_capture_04.cpp
@@ -6,7 +6,7 @@ int main()
 	int y = 20;
 
 	auto f = [&x = x, y = x + 5]{ return ++x * y; };
-	std::cout << f() << "\n";
-	std::cout << "x = " << x << "\n";
-	std::cout << "y = " << y << "\n";
+	std::cout << f() << '\n';
+	std::cout << "x = " << x << '\n';
+	std::cout << "y = " << y << '\n';
 }
