a4156d7d2c225230b5484550ab4d778d677911ff
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-09 17:22:33 +0300

    Create ci_05.cpp

---
+++ 3324_CONSTINIT/ci_05.cpp
@@ -0,0 +1,11 @@
+#include <utility>
+#include <string>
+#include <vector>
+#include <array>
+
+constinit std::pair p = { 15, "ali" };
+constinit std::array ar = { 3, 6, 8, 1 };
+//constinit std::string name{ "alican" }; //error
+//constinit std::vector ivec{1, 4, 5, 6}; //error
+
+
