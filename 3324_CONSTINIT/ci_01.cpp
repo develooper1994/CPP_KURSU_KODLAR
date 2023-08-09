b637822d960e72de72a4415df321d183ead40b1d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-09 16:39:29 +0300

    Update ci_01.cpp

--- 3324_CONSTINIT/ci_01.cpp
+++ 3324_CONSTINIT/ci_01.cpp
@@ -1,1 +1,13 @@
-henüz hazır değil
+constinit auto g = 56;
+
+int get_count() 
+{
+	static constinit int count{};
+	return ++count;
+}
+
+class Nec {
+public:
+	inline static constinit int sval = 1000;
+};
+
