33c44b9a217b5f06b43da151f9bb76a31bfba4d0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-10 10:29:54 +0300

    Update format_03.cpp

--- 3350_FORMAT/format_03.cpp
+++ 3350_FORMAT/format_03.cpp
@@ -6,13 +6,13 @@ int main()
 	using namespace std;
 
 	int x{};
-	//cout << format("{}", &x) << "\n"; // geçersiz.
-	//cout << format("{:p}", &x) << "\n"; // geçersiz
+	//cout << format("{}", &x) << '\n'; // error.
+	//cout << format("{:p}", &x) << '\n'; // error.
 
-	cout << format("{}", (void*)&x) << "\n"; 
-	cout << format("{:p}", (void*)&x) << "\n"; 
-	cout << format("{:p}", static_cast<void*>(&x)) << "\n";  // 
+	cout << format("{}", (void*)&x) << '\n';
+	cout << format("{:p}", (void*)&x) << '\n';
+	cout << format("{:p}", static_cast<void*>(&x)) << '\n';  // 
 
-	cout << format("{}", nullptr) << "\n"; // 0x0
-	cout << format("{:p}", nullptr) << "\n"; // 0x0
+	cout << format("{}", nullptr) << '\n'; // 0x0
+	cout << format("{:p}", nullptr) << '\n'; // 0x0
 }
