e7c878e92c6d1b34b68951ee8ab454b8576c8b21
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-10 10:37:12 +0300

    Update format_06.cpp

--- 3350_FORMAT/format_06.cpp
+++ 3350_FORMAT/format_06.cpp
@@ -4,9 +4,7 @@
 
 int main()
 {
-
 	using namespace std;
-
 	cout << format(locale{ "turkish" }, "{:L}", 3.12) << '\n';  // 3,12
 	cout << format(locale{ "turkish" }, "{:L}", 456'877'123) << '\n';  //   456.877.123
 	cout << format(locale{ "turkish" }, "{}", 456'877'123) << '\n';  //  456877123
