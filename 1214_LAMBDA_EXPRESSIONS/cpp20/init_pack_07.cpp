4238ba7291ceff70ac1d51368ab52822ed994470
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-12 06:35:07 +0300

    Delete init_pack_07.cpp

--- 1214_LAMBDA_EXPRESSIONS/cpp20/init_pack_07.cpp
+++
@@ -1,9 +0,0 @@
-#include <functional>
-#include <utility>
-
-template <class... Args>
-auto delay_invoke_foo(Args... args) {
-    return[...args = std::move(args)]() -> decltype(auto) {
-        return foo(args...);
-    };
-}
