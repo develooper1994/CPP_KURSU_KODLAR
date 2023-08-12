272bd6b3bbadbf5f8cf7e4c42a50b34603d1cb2b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-12 06:34:54 +0300

    Update init_pack_06.cpp

--- 1214_LAMBDA_EXPRESSIONS/cpp20/init_pack_06.cpp
+++ 1214_LAMBDA_EXPRESSIONS/cpp20/init_pack_06.cpp
@@ -1,3 +1,7 @@
+#include <tuple>
+
+//before C++20
+
 template <class... Args>
 auto delay_invoke_foo(Args... args) {
     return[tup = std::make_tuple(std::move(args)...)]() -> decltype(auto) {
@@ -6,3 +10,15 @@ auto delay_invoke_foo(Args... args) {
             }, tup);
     };
 }
+
+//since C++20
+#include <functional>
+#include <utility>
+
+template <class... Args>
+auto delay_invoke_foo(Args... args) {
+    return[...args = std::move(args)]() -> decltype(auto) {
+        return foo(args...);
+    };
+}
+
