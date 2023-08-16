7e66c65f0d953ea928e58ed182b809d71941db4c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-16 17:21:17 +0300

    Create c_01.cpp

---
+++ 3160_CONST/constexpr/c_01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <array>
+#include <bitset>
+
+constexpr int sum_square(int x, int y)
+{
+	return x * x + y * y;
+}
+
+
+int main()
+{
+	constexpr int a = 10;
+	constexpr int b = 20;
+
+	int ar[sum_square(a, b)]{}; // int ar[500]{};
+	static_assert(sum_square(3, 5) == 34); 
+	std::array<int, sum_square(a, b)> ar500{};
+	std::bitset<sum_square(4, 5)> bs{};
+}
