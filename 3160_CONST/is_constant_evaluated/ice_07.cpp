a85ba509a91d0515f0c06b8698c8adb95dc3e3d6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-16 15:45:47 +0300

    Create ice_07.cpp

---
+++ 3160_CONST/is_constant_evaluated/ice_07.cpp
@@ -0,0 +1,31 @@
+#include <type_traits>
+#include <iostream>
+
+bool f_runtime() 
+{
+	return std::is_constant_evaluated(); // always false
+}
+
+constexpr bool f_constexpr() 
+{
+	return std::is_constant_evaluated(); // may be false or true
+}
+
+consteval bool f_consteval() 
+{
+	return std::is_constant_evaluated(); // always true
+}
+
+int main()
+{
+	boolalpha(std::cout);
+
+	static bool	b1 = f_runtime();	// false
+	std::cout << "b1 = " << b1 << '\n';
+
+	static bool b2 = f_constexpr(); // true
+	std::cout << "b2 = " << b2 << '\n';
+
+	static bool b3 = f_consteval(); // true
+	std::cout << "b3 = " << b3 << '\n';
+}
