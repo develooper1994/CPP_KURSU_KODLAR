3a8ae704c29df96e2d8c6b28def803129247653b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-16 15:27:39 +0300

    Create ice_04.cpp

---
+++ 3160_CONST/is_constant_evaluated/ice_04.cpp
@@ -0,0 +1,24 @@
+#include <type_traits>
+#include <iostream>
+
+constexpr bool is_const_eval() 
+{
+	return std::is_constant_evaluated();
+}
+
+bool g1			= is_const_eval(); // true
+const bool g2	= is_const_eval(); // true
+static bool g3	= is_const_eval(); // true
+static int g4	= g1 + is_const_eval(); // false
+
+int main()
+{
+	using namespace std;
+
+	boolalpha(cout);
+
+	cout << "g1 = " << g1 << '\n';
+	cout << "g2 = " << g2 << '\n';
+	cout << "g3 = " << g3 << '\n';
+	cout << "g4 = " << g4 << '\n';
+}
