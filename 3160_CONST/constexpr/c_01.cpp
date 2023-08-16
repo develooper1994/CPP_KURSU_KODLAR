a6113b40f06baaf4604188a5bd470cfe3381a046
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-16 17:26:51 +0300

    Update c_01.cpp

--- 3160_CONST/constexpr/c_01.cpp
+++ 3160_CONST/constexpr/c_01.cpp
@@ -13,6 +13,7 @@ int main()
 	constexpr int a = 10;
 	constexpr int b = 20;
 
+	constexpr int c = sum_square(a, b);
 	int ar[sum_square(a, b)]{}; // int ar[500]{};
 	static_assert(sum_square(3, 5) == 34); 
 	std::array<int, sum_square(a, b)> ar500{};
