0304e833f323edfee72935fc418d6d792fbb5ce4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-17 00:09:36 +0300

    Create a_01.cpp

---
+++ 3160_CONST/constexpr/allocation/a_01.cpp
@@ -0,0 +1,26 @@
+#include <numeric>
+
+constexpr long long factorial(int n)
+{
+	return n < 2 ? 1 :n * factorial(n - 1);
+}
+
+constexpr double get_e(int n)
+{
+	double* p = new double[n];
+	for (int i = 0; i < n; ++i) {
+		p[i] = 1. / factorial(i);
+	}
+
+	auto sum = std::accumulate(p, p + n, 0.);
+
+	delete p;
+
+	return sum;
+}
+
+
+int main()
+{
+	constexpr auto val = get_e(15);
+}
