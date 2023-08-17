570e04005606e32043b9d1c20859e1c22957083c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-17 16:34:45 +0300

    Create di_06.cpp

---
+++ 3800_CPP20/designated_initializer/di_06.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+struct Nec {
+	int x;
+	int y{3}; 
+	int z;
+};
+
+Nec get_nec()
+{
+	return Nec{.x = 1, .z = 5};
+}
+
+int main()
+{
+	auto nec = get_nec();
+
+	std::cout << nec.x << nec.y << nec.z << '\n';
+}
