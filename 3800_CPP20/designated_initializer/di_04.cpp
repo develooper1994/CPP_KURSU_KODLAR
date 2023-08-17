13316da97c2bceab781c45b5d74e459cdd93cfd9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-17 16:04:37 +0300

    Update di_04.cpp

--- 3800_CPP20/designated_initializer/di_04.cpp
+++ 3800_CPP20/designated_initializer/di_04.cpp
@@ -1,24 +1,22 @@
-struct Time
-{
-	int min;
-	int hour;
+struct Time {
+  int min;
+  int hour;
 };
 
-struct Date
-{
-	int year;
-	int month;
-	int day;
-	Time time;
-	static int hmode;
+struct Date {
+  int year;
+  int month;
+  int day;
+  Time time;
+  static int hmode;
 };
 
-int main()
+int main() 
 {
-	//Date d1 = { .hmode = 0 }; // invalid - static member
-	//Date d2 = { .month = 3, .year = 1998 }; // invalid - wrong order
-	//Date d3 = { 3, .year = 1998 }; // invalid - mix init
-	//Date d4 = {.time.min = 25; // invalid. Nested member access is now allowed.};
+  // Date d1 = { .hmode = 0 }; // invalid - static member
+  // Date d2 = { .month = 3, .year = 1998 }; // invalid - wrong order
+  // Date d3 = { 3, .year = 1998 }; // invalid - mix init
+  // Date d4 = {.time.min = 25; // invalid. Nested member access is not allowed.};
 
-	Date d5 = { .time = {32, 4} }; // valid
+  Date d5 = {.time = {32, 4}}; // valid
 }
