e0e4ce9c71c0a75d3b95e1eba9304cb114c4659e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-18 18:26:33 +0300

    Update and rename agg09.cpp to direct_04.cpp

--- 3800_CPP20/aggregates/direct_04.cpp
+++ 3800_CPP20/aggregates/direct_04.cpp
@@ -1,6 +1,9 @@
+#include <memory>
+#include <vector>
+
 struct Nec {
 	int a, b;
-	
+
 	struct Nested {
 		int x;
 	}c;
@@ -11,10 +14,7 @@ int main()
 	using namespace std;
 
 	auto p = make_unique<Nec>(1, 2, Nec::Nested{ 3 }); //Valid at C++20. Invalid at C++17
-	
+
 	vector<Nec> nvec;
 	nvec.emplace_back(1, 2, Nec::Nested{ 3 });  //Valid at C++20. Invalid at C++17
 }
-
-
-//-----------------------------------------------------------------------------------------------------------------
