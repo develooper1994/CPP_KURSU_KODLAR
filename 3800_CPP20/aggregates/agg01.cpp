555a298931940d8c65fd8d86a6ff136835d4425c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-18 16:26:50 +0300

    Update agg01.cpp

--- 3800_CPP20/aggregates/agg01.cpp
+++ 3800_CPP20/aggregates/agg01.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
+#include <type_traits>
 
 struct Nec {
 	int x, y;
+
 	Nec() = default; // user declared defaulted
 
-	int a = 23; 
+	int a = 23;
 };
 
 int main()
 {
-	Nec n1{ 1,2 }; // Valid in C++17. Invalid in C++20 
+	Nec n1{ 1, 2 }; // Valid in C++17. Invalid in C++20 
+
+	static_assert(std::is_aggregate_v<Nec>); //error
 }
