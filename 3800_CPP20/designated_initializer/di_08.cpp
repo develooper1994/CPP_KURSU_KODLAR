a6bded7ee7926d6c6b2b39365d24cd57663e3094
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-18 17:30:37 +0300

    Update di_08.cpp

--- 3800_CPP20/designated_initializer/di_08.cpp
+++ 3800_CPP20/designated_initializer/di_08.cpp
@@ -1,17 +1,23 @@
 #include <vector>
+#include <iostream>
 
 struct Point {
-  double x, y, z;
+	double x, y, z;
 };
 
-int main() 
+int main()
 {
-  Point a{};
-  a.x = 3.4;
+	Point a{};
+	a.x = 3.4;
 
-  Point p = {.x = 3, .y = 5, .z = 5};
-  std::vector<Point> pvec;
+	Point p = { .x = 3, .y = 5, .z = 5 };
+	std::vector<Point> pvec;
 
-  pvec.push_back(Point{.x = 1.1, .y = 2.2, .z = 3.3});
-  pvec.push_back(Point{.x = {1.1}, .y = {2.2}, .z = {3.3}});
+	pvec.push_back(Point{ .x = 1.1, .y = 2.2, .z = 3.3 });
+	pvec.push_back(Point{ .x = {1.1}, .y = {2.2}, .z = {3.3} });
+	pvec.push_back(a);
+	pvec.push_back(p);
+
+	for (const auto& [x, y, z] : pvec)
+		std::cout << x << ' ' << y << ' ' << z << '\n';
 }
