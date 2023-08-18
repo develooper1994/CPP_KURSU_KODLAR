e4f163642f7f5c3002d85b38f2180e503b7fb853
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-18 11:13:22 +0300

    Create ag_01.cpp

---
+++ 3100_AGGREGATES/ag_01.cpp
@@ -0,0 +1,25 @@
+#include <type_traits>
+#include <string>
+
+
+class A {
+public:
+	int x, y, z;
+	std::string str;
+};
+
+class B {
+	int x;
+};
+
+class B {
+protected:
+	int x;
+};
+
+int main()
+{
+	static_assert(std::is_aggregate_v<A>, "not an aggregate type");
+	//static_assert(std::is_aggregate_v<B>, "not an aggregate type");
+	//static_assert(std::is_aggregate_v<C>, "not an aggregate type");
+}
