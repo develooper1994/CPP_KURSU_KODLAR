b30dcc1848a4cbd55a7dc9a34a77ba02ee672266
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-21 15:55:51 +0300

    Create jossutis_01.cpp

---
+++ 3370_CONCEPTS/requires_expression/jossutis_01.cpp
@@ -0,0 +1,43 @@
+// From Nicholai Jossutis C++20 book.
+// http://cppstd20.com/code/lang/maxvalue.cpp.html
+#include <iostream>
+
+// concept for pointer-like objects:
+template<typename T>
+concept IsPointer = requires(T p) {
+    *p;             // operator * has to be valid
+    p == nullptr;   // can compare with nullptr
+    {p < p} -> std::convertible_to<bool>;  // < yields bool
+};
+
+// maxValue() for plain values:
+auto maxValue(auto a, auto b)
+{
+    return b < a ? a : b;
+}
+
+// maxValue() for pointers:
+auto maxValue(IsPointer auto a, IsPointer auto b)
+    requires std::totally_ordered_with<decltype(*a), decltype(*b)>
+{
+    return maxValue(*a, *b);  // return maximum value of where the pointers refer to
+}
+
+int main()
+{
+    int x = 42;
+    int y = 77;
+    std::cout << maxValue(x, y) << '\n';      // maximum value of ints
+    std::cout << maxValue(&x, &y) << '\n';    // maximum value of where the pointers point to
+
+    int* xp = &x;
+    int* yp = &y;
+    std::cout << maxValue(&xp, &yp) << '\n';  // maximum value of pointer to pointer
+
+    double d = 49.9;
+    std::cout << maxValue(xp, &d) << '\n';    // maximum value of int and double pointer
+}
+
+
+
+  
