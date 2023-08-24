eadb35321cdbc07c3348784f50048b79118f2350
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-08-24 17:26:00 +0300

    Update and rename 3370_CONCEPTS/co_02.cpp to 3370_CONCEPTS/concepts/cnc_02.cpp

--- 3370_CONCEPTS/concepts/cnc_02.cpp
+++ 3370_CONCEPTS/concepts/cnc_02.cpp
@@ -1,42 +1,45 @@
 #include <concepts>
 #include <iostream>
 
-template <typename T> requires std::integral<T>
-T gcd1(T a, T b) 
+template <typename T> 
+requires std::integral<T>  //requires clause
+T gcd1(T a, T b)
 {
-    if (b == 0) 
+    if (b == 0)
         return a;
-    
+
     return gcd1(b, a % b);
 }
 
 
-template<std::integral T>
-T gcd2(T a, T b) 
+template<std::integral T> //constraine template parameter
+T gcd2(T a, T b)
 {
-    if (b == 0) 
+    if (b == 0)
         return a;
-    
+
     return gcd2(b, a % b);
 }
 
-std::integral auto gcd3(std::integral auto a, std::integral auto b) 
+//abbreviated (constrained) template syntax
+std::integral auto gcd3(std::integral auto a, std::integral auto b)
 {
-    if (b == 0) 
+    if (b == 0)
         return a;
-    
+
     return gcd3(b, a % b);
 }
 
-auto gcd4(auto a, auto b) 
+//abbreviated (unconstrained) template syntax
+auto gcd4(auto a, auto b)
 {
-    if (b == 0) 
+    if (b == 0)
         return a;
-    
+
     return gcd4(b, a % b);
 }
 
-int main() 
+int main()
 {
 
     std::cout << "gcd1(40, 28)= " << gcd1(40, 28) << '\n';
