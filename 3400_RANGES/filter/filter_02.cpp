5a286034795aadeaeb550a3bf62d38bd0b76261f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-05 15:44:37 +0300

    Update filter_02.cpp

--- 3400_RANGES/filter/filter_02.cpp
+++ 3400_RANGES/filter/filter_02.cpp
@@ -1,22 +1,19 @@
 #include <vector>
 #include <string>
-#include "nutility.h" //print
 #include <ranges>
+#include <iostream>
 
 int main()
 {
 	using namespace std;
 
-	vector<string> svec;
-	rfill(svec, 30, rname);
-
-	print(svec);
-
+	vector<string> svec{ "ali", "mert", "can", "zeynep", "melike", "aykut", "necati", "tamer", "emre", "ahmet"};
+	
 	char c;
 
-	cout << "icinde hangi karkaterler olanlar : ";
+	cout << "icinde hangi karakter olanlar: ";
 	cin >> c;
-
-	for (const auto& s : views::filter(svec, [c](const auto& s) { return s.find(c) != string::npos; }))
-		cout << s << '\n'; // İçerisinde belirli bir karakter olanları yazdırdık
+		
+	for (const auto& s : views::filter(svec, [c](const auto& s) { return s.contains(c); }))
+		cout << s << ' '; // Uzunluğu çift olanlar yazılacak
 }
