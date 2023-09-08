4b5819b4f319ef775d6f2767fb57c2912c14e799
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-08 06:28:11 +0300

    Update iota_01.cpp

--- 3400_RANGES/views/iota/iota_01.cpp
+++ 3400_RANGES/views/iota/iota_01.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	auto v = std::views::iota(10, 20);
-	for (auto x : v) {
-		std::cout << x << " "; //10 11 12 13 14 15 16 17 18 19
+	for (auto i : v) {
+		std::cout << i << ' '; //10 11 12 13 14 15 16 17 18 19
 	}
 }
