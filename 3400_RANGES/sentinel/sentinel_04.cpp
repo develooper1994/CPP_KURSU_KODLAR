e9dced0967f65ce3dc674a2bc3b617d16a024668
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-10 08:14:21 +0300

    Update sentinel_04.cpp

--- 3400_RANGES/sentinel/sentinel_04.cpp
+++ 3400_RANGES/sentinel/sentinel_04.cpp
@@ -3,19 +3,19 @@
 #include <string>
 #include <algorithm>
 
-struct UnreachableSentinel {
+struct UnreachableSentinel_t {
 	constexpr bool operator==(const auto& val) const
 	{
 		return false;
 	}
 };
 
-constexpr UnreachableSentinel UnReachableSentinel_t;
+constexpr UnreachableSentinel_t UnReachableSentinel_t;
 
 
 /*
 	find algoritması içinde
-	while (*beg != UnReachableSentinel_t)  //always true olacak
+	while (*beg != UnReachableSentinel)  //always true olacak
 */
 
 std::string read_from_file(const char* pfile_name);
@@ -23,5 +23,5 @@ std::string read_from_file(const char* pfile_name);
 int main()
 {
 	const auto str = read_from_file("bigfile.txt");
-	auto iter = std::ranges::find(str.begin(), UnReachableSentinel_t, '\n');
+	auto iter = std::ranges::find(str.begin(), UnReachableSentinel, '\n');
 }
