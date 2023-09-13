586fc6183e71cd52f7b054887e7e3a418b51e8b2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-13 18:10:00 +0300

    Delete 0340_NAMESPACE directory

--- 0340_NAMESPACE/inline/inline_namespace_03.cpp
+++
@@ -1,8 +0,0 @@
-namespace A::B:: inline C { //C++20
-	int x;
-}
-
-int main()
-{
-	A::B::x = 5;
-}
--- 0340_NAMESPACE/using_decl/using_01.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-
-int main()
-{
-	using std::string, std::cout;
-
-	cout << string{ "necati" };
-}
