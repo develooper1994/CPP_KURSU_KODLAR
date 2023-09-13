5c646eaaf47c8d393acab76f04a53d43bc63b75c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-13 12:03:24 +0300

    Delete 3400_RANGES/views/common/common_01.cpp

--- 3400_RANGES/views/common/common_01.cpp
+++
@@ -1,6 +0,0 @@
-template<typename BegT, typename EndT>
-void callAlgo(BegT beg, EndT end)
-{
-	auto v = std::views::common(std::ranges::subrange(beg, end));
-	algo(v.begin(), v.end()); // assume algo() requires iterators with the same type
-}
